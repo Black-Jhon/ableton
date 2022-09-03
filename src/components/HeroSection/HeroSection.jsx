7256325cf7a77714864fdbe2bca92fd8ac1c29b8
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-09-03 23:22:51 +0100

    updated the hero area

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <div className="lg:mx-32">
       <div className="relative">
         <img src={heroImage} alt="" className="w-full object-cover h-[48rem]" />
-        <span className="absolute text-9xl lg:top-[20rem] lg:right-[26rem] capitalize text-[#fd5948] font-bold font-mono">ableton</span>
+        <span className="absolute text-9xl lg:top-[20rem] lg:right-[26%]  capitalize text-[#fd5948] font-bold font-mono">ableton</span>
       </div>
 
       <div>
