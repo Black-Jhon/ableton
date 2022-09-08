593cd8bbe669f46036e2a524bad30307288e34ff
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-09-08 04:31:12 +0100

    Updated ThirdSection.jsx

--- src/components/ThirdSection/ThirdSection.jsx
+++ src/components/ThirdSection/ThirdSection.jsx
@@ -5,12 +5,12 @@ import right from "../../assets/images/ThirdOne.jpg";
 const ThirdSection = () => {
   return (
     <div className="my-24 lg:ml-32 flex relative">
-      <div className="absolute max-w-[40rem] top-28">
+      <div className="absolute lg:max-w-[40rem] md:max-w-[23rem] max-w-[10rem] lg:left-0 left-8 lg:top-28 top-10">
         <img src={left} className="object-cover" alt="" />
       </div>
 
-      <div className="bg-[#fbffa7] max-w-[70%] h-[43rem] ml-[30%]">
-        <div className="max-w-[50%] lg:mt-40 lg:ml-96">
+      <div className="bg-[#fbffa7] max-w-[70%] lg:h-[43rem] md:h-80 h-48 ml-[30%]">
+        <div className="max-w-[50%] lg:mt-40 md:mt-20 mt-12 lg:ml-96 md:ml-52 ml-24">
           <img src={right} className="object-cover" alt="" />
         </div>
       </div>
