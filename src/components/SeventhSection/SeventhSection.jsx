ffb6edb25d10b918d0b8ba97f2c8ce7d6225d0ed
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-09-08 06:35:42 +0100

    Updated SeventhSection.jsx and SixthSection.jsx

--- src/components/SeventhSection/SeventhSection.jsx
+++ src/components/SeventhSection/SeventhSection.jsx
@@ -5,14 +5,28 @@ import { BiChevronRight } from "react-icons/bi";
 const SeventhSection = () => {
   return (
     <div>
-      <div className="lg:grid grid-cols-2 lg:mx-32 my-40">
-        <div className="">
-          <img src={seven} className="h-[40rem] object-cover" alt="" />
+      <div className="lg:my-40">
+        <div className="flex justify-center">
+          <h1 className="lg:w-[50rem] md:w-[40rem] w-80 lg:text-xl md:text-lg text-sm lg:tracking-wider lg:leading-loose font-bold font-mono">
+            We want our employees to love it here. Since we’re looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible.{" "}
+          </h1>
         </div>
 
-        <div className="bg-[#b1c5ff] h-[40rem] flex justify-center items-center">
-          <div className="grid gap-5">
-            <div className="w-[25rem] text-xl tracking-wider leading-loose font-bold font-mono">
+        <p className="lg:w-[50rem] md:w-[40rem] lg:text-inherit md:text-lg text-sm w-80 flex m-auto mt-5 font-thin lg:leading-loose lg:tracking-wider">
+          If you're joining us in Berlin, we'll help with relocation and paperwork. We’ll even provide you with free German or English lessons. Plus, working in Germany means you can expect
+          comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it’s not all work; we have several company and team
+          outings throughout the year as well as a variety of fun, informal small-group activities.
+        </p>
+      </div>
+
+      <div className="md:grid grid-cols-2 lg:mx-32 md:mx-24 lg:my-40 my-10">
+        <div className="">
+          <img src={seven} className="lg:h-[40rem] object-cover" alt="" />
+        </div>
+
+        <div className="bg-[#b1c5ff] lg:h-[40rem] flex justify-center items-center">
+          <div className="grid gap-5 lg:my-0 my-10">
+            <div className="lg:w-[25rem] md:w-72 w-80 lg:text-xl lg:tracking-wider lg:leading-loose font-bold font-mono">
               We’re really proud of the work we’ve done so far. But there’s so much more to come. If you’d like to be a part of it, please join us.
             </div>
 
--- src/components/SixthSection/SixthSection.jsx
+++ src/components/SixthSection/SixthSection.jsx
@@ -5,28 +5,28 @@ import sixb from "../../assets/images/sixb.jpg";
 const SixthSection = () => {
   return (
     <div>
-      <div className="my-40">
+      <div className="lg:my-40">
         <div className="flex justify-center">
-          <h1 className="w-[50rem] text-xl tracking-wider leading-loose font-bold font-mono">
-            We want our employees to love it here. Since we’re looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible.{" "}
+          <h1 className="lg:w-[50rem] md:w-[40rem] w-80 lg:text-xl md:text-lg text-sm lg:tracking-wider lg:leading-loose font-bold font-mono">
+            We’re passionate about what we do, but we’re equally passionate about improving who we are.
           </h1>
         </div>
 
-        <p className="w-[50rem] flex m-auto mt-5 font-thin leading-loose tracking-wider">
-          If you're joining us in Berlin, we'll help with relocation and paperwork. We’ll even provide you with free German or English lessons. Plus, working in Germany means you can expect
-          comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it’s not all work; we have several company and team
-          outings throughout the year as well as a variety of fun, informal small-group activities.
+        <p className="lg:w-[50rem] md:w-[40rem] lg:text-inherit md:text-lg text-sm w-80 flex m-auto mt-5 font-thin lg:leading-loose lg:tracking-wider">
+          We work hard to foster an environment where people can grow both personally and professionally, and we strive to create a wealth of opportunities to learn from and with each other.
+          <br /> <br /> Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition,
+          staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.
         </p>
       </div>
 
       <div className="my-24 lg:mr-32 flex relative">
-        <div className="bg-[#d5b3ff] w-[65%]">
-          <div className="grid gap-32 max-w-[50%] h-[80%] object-cover lg:ml-32 mb-32">
-            <img src={sixa} className="mt-32" alt="" />
+        <div className="bg-[#d5b3ff] w-[65%] lg:h-auto h-96">
+          <div className="lg:grid flex lg:h-auto h-80 lg:gap-32 lg:max-w-[50%] max-w-[60%] object-cover lg:ml-32 md:ml-28 ml-5 mb-32">
+            <img src={sixa} className="lg:mt-32 mt-16" alt="" />
           </div>
         </div>
 
-        <div className="max-w-[40rem] top-72 absolute lg:ml-[55%]">
+        <div className="lg:max-w-[40rem] max-w-[10rem] md:max-w-[20rem] lg:top-72 md:top-20 top-36  absolute lg:ml-[55%] md:ml-96 ml-48">
           <img src={sixb} className="" alt="" />
         </div>
       </div>
