27b7c99327307394ce0242aabc08204446da421d
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-09-08 04:05:22 +0100

    Updated HeroDescription.jsx and HeroSection.jsx

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <div className="lg:mx-32">
       <div className="relative">
         <img src={heroImage} alt="" className="w-full object-cover h-[48rem]" />
-        <span className="absolute text-9xl lg:top-[20rem] lg:right-[26%]  capitalize text-[#fd5948] font-bold font-mono">ableton</span>
+        <span className="absolute lg:text-9xl md:text-6xl text-5xl lg:top-[20rem] top-80 lg:right-[26%] md:right-72 right-20  capitalize text-[#fd5948] font-bold font-mono">ableton</span>
       </div>
 
       <div>
